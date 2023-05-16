841c9a4a9543c4ee28f41dfd1b7c1aa9c728f698
everton <evertonrichard444@gmail.com>
2023-05-16 16:09:29 -0300

    exercicios javascript

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60){ //condição simples
+    console.log('Voce está multado')
+} 
+console.log('Dirija sempre o sinto de segurança!')
---
+++ aula 11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA!'
+console.log (`vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log ('você é Brasileiro!')
+} else {
+    console.log ("Você é estrangeiro")
+}
